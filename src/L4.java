8c88454a9e53ee0608d16da3bd70885ca35eb182
solchtyl001 <solchtyl001@warren.k12.in.us>
2021-08-09 13:07:27 -0400

    1.3-1.5 notes

--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,29 @@
 public class L4 {
+    public static void main(String[] args) {
+
+        int value = 5;
+
+        // Declare A variable- stating dadatype identifyer
+
+        double total;
+        String message = "hi";
+        final double GRAVITY= -9.8;
+        // assign a value
+        total = 6;
+
+
+        total=10;
+        message = "bye";
+        //Gravity = -10;  DIS IS BAAAAAD
+
+
+
+
+
+
+    }
+
+
+
+
 }
--- src/L5.java
+++ src/L5.java
@@ -1,2 +1,34 @@
 public class L5 {
+
+
+    public static void main(String[] args) {
+
+       /* double basePay = 7.25;
+        double hoursWeek1 = 16.5;
+        double hoursWeek2 = 23.75;
+
+        double totalHours = hoursWeek1 + hoursWeek2;
+        double wages = basePay * totalHours;*/
+
+        double fraction = 1 / 2.0;
+        System.out.println(fraction);
+
+        //Modulus is the remainder of a division problem
+        // 25%6+1
+        //31%5=1
+
+
+        //13
+        //2
+        //4
+        //10
+        //55
+        //
+
+
+
+
+
+
+    }
 }
