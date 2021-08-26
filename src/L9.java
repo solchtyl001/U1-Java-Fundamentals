1953a4abff897a42582aaa3ccc460d7b95a4d2d7
solchtyl001 <solchtyl001@warren.k12.in.us>
2021-08-26 13:15:02 -0400

    N O T E S

--- src/L9.java
+++ src/L9.java
@@ -1,3 +1,35 @@
+import javax.swing.JOptionPane;
+
 public class L9{
+
+    public static void main(String[] args) {
+
+
+        String firstname = JOptionPane.showInputDialog("What is your name?");
+        String lastname = JOptionPane.showInputDialog("What last name is?");
+
+
+
+        JOptionPane.showMessageDialog(null, "Howdy " + firstname + "+" + lastname+ "!!");
+
+
+
+
+      System.exit(0);
+
+    //ALL INPUT IS RECEIVED AS A STRING
+        // PRO TIP: Variables of same data type can be separated with a comma in declaration
+
+
+
+
+
+
+
+
+    }
+
+
+
 
 }
